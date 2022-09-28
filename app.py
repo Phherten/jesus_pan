373981600e42004753909003255454232101d5fb
Phherten <98804730+Phherten@users.noreply.github.com>
2022-09-28 19:06:04 +0200

    casi acabado

--- app.py
+++ app.py
@@ -1,21 +1,56 @@
 from flask import Flask, request, jsonify
-
-from funciones import devolver_variables, agregar_datos
+import requests
+from funciones import capturar_texto, consultar_mes, resumen
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/añadir_km', methods=['POST'])
-def agregar():  # put application's code here
-    request_body = request.get_json()
-    agregar_datos(request_body['km'])
-    return jsonify({"mensaje": "km añadido"})
-
-
 @app.route('/')
 def km():
-    valor = devolver_variables()
-    return jsonify({"km": valor})
+    valor = consultar_mes()
+    return valor
+
+
+@app.route('/consultar', methods=['GET'])
+def consultar():
+    url = "https://api.telegram.org/bot5534433351:AAGQnST7oEFiIq6IhMh8nfEuL3UoNa0Ux-8/getUpdates"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    json_updates = json.loads(response.text)
+    text = json_updates["result"][-1]["message"]["text"]
+    capturar_texto(text)
+    return "Mensaje Recibido"
+
+
+@app.route('/avisar', methods=['POST'])
+def avisar():
+    request_body = request.get_json()
+    bot_token = request_body["token"]
+    bot_chat_id = "611152189"
+    msg = "Mete los Kilometros"
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode=Markdown&text=' + msg
+    response = requests.get(send_text)
+    return "Mensaje Enviado"
+
+
+@app.route('/resumen_mes', methods=['POST'])
+def resumen_mes():
+    km_ruta, media_ruta, media_ida, media_vuelta, km_totales = resumen()
+    request_body = request.get_json()
+    bot_token = request_body["token"]
+    bot_chat_id = "611152189"
+    msg = f'Este mes has hecho {km_ruta}km en la ruta, una media de {media_ruta}km diarios,en ir al almacen una media ' \
+          f'de {media_ida}km desde casa y en la vuelta una media de {media_vuelta}km, lo que hacen un tot' \
+          f'al de {km_totales}km. '
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode' \
+                                                                                                     '=Markdown&text=' + msg
+    response = requests.get(send_text)
+
+    return "Resumen enviado"
 
 
 if __name__ == '__main__':
--- funciones.py
+++ funciones.py
@@ -1,12 +1,34 @@
-km_casa_almacen = []
-km_ruta = []
-km_ultima_casa = []
-conductor = []
+from datetime import date
+
+resumen_mes = []
+hoy = date.today()
 
 
-def agregar_datos(km):
-    km_ruta.append(km)
+def capturar_texto(texto):
+    array_texto = texto.split(" ")
+    resumen_mes.append(
+        {"Fecha": hoy.day, "Conductor": array_texto[0], "Km_ruta": array_texto[1], "km_a_almacen": array_texto[2],
+         "km_a_casa": array_texto[3]})
 
 
-def devolver_variables():
-    return km_ruta
+def consultar_mes():
+    return resumen_mes
+
+
+def resumen():
+    km_ruta = 0
+    dias_mes = 0
+    km_ida = 0
+    km_vuelta = 0
+    for km in resumen_mes:
+        km_ruta += int(km['Km_ruta'])
+        km_ida += int(km['km_a_almacen'])
+        km_vuelta += int(km['km_a_casa'])
+        dias_mes += 1
+
+    media_ruta = km_ruta / dias_mes
+    media_ida = km_ida / dias_mes
+    media_vuelta = km_vuelta / dias_mes
+    km_totales = km_ruta + km_ida + km_vuelta
+
+    return km_ruta, media_ruta, media_ida, media_vuelta, km_totales
