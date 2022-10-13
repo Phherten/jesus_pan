d69fefc421cc84b78efccf6b4bdb6484f6368386
Phherten <98804730+Phherten@users.noreply.github.com>
2022-10-13 09:07:34 +0200

    CAMBIO A PELI DEL DIA

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, jsonify
 import requests
-from funciones import capturar_texto, consultar_mes, resumen, resetear
+from funciones import add_film, day_film, films
 import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def km():
-    valor = consultar_mes()
+def consultar():
+    valor = films()
     return valor
 
 
-@app.route('/consultar', methods=['POST'])
-def consultar():
+@app.route('/add_films', methods=['POST'])
+def add_films():
     request_body = request.get_json()
     url = f'https://api.telegram.org/bot{request_body["token"]}/getUpdates'
 
@@ -23,36 +23,20 @@ def consultar():
     response = requests.request("GET", url, headers=headers, data=payload)
     json_updates = json.loads(response.text)
     text = json_updates["result"][-1]["message"]["text"]
-    capturar_texto(text)
+    add_film(text)
     return "Mensaje Recibido"
 
 
-@app.route('/avisar', methods=['POST'])
-def avisar():
+@app.route('/day_film', methods=['POST'])
+def day_film():
     request_body = request.get_json()
     bot_token = request_body["token"]
-    bot_chat_id = "611152189"
-    msg = "Mete los Kilometros"
+    bot_chat_id = request_body["chat_id"]
+    film = day_film()
+    msg = film
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode=Markdown&text=' + msg
     response = requests.get(send_text)
     return "Mensaje Enviado"
-
-
-@app.route('/resumen_mes', methods=['POST'])
-def resumen_mes():
-    km_ruta, media_ruta, media_ida, media_vuelta, km_totales = resumen()
-    request_body = request.get_json()
-    bot_token = request_body["token"]
-    bot_chat_id = "611152189"
-    msg = f'Este mes has hecho {km_ruta}km en la ruta, una media de {media_ruta}km diarios,en ir al almacen una media ' \
-          f'de {media_ida}km desde casa y en la vuelta una media de {media_vuelta}km, lo que hacen un tot' \
-          f'al de {km_totales}km. '
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode' \
-                                                                                                     '=Markdown&text=' + msg
-    response = requests.get(send_text)
-    resetear()
-
-    return "Resumen enviado"
 
 
 if __name__ == '__main__':
--- funciones.py
+++ funciones.py
@@ -1,38 +1,20 @@
 from datetime import date
+from random import randint
 
-resumen_mes = []
-hoy = date.today()
-
-
-def capturar_texto(texto):
-    array_texto = texto.split(" ")
-    resumen_mes.append(
-        {"Fecha": hoy.day, "Conductor": array_texto[0], "Km_ruta": array_texto[1], "km_a_almacen": array_texto[2],
-         "km_a_casa": array_texto[3]})
+peliculas = []
 
 
-def consultar_mes():
-    return resumen_mes
+def films():
+    return peliculas
 
 
-def resumen():
-    km_ruta = 0
-    dias_mes = 0
-    km_ida = 0
-    km_vuelta = 0
-    for km in resumen_mes:
-        km_ruta += int(km['Km_ruta'])
-        km_ida += int(km['km_a_almacen'])
-        km_vuelta += int(km['km_a_casa'])
-        dias_mes += 1
-
-    media_ruta = km_ruta / dias_mes
-    media_ida = km_ida / dias_mes
-    media_vuelta = km_vuelta / dias_mes
-    km_totales = km_ruta + km_ida + km_vuelta
-
-    return km_ruta, media_ruta, media_ida, media_vuelta, km_totales
+def add_film(texto):
+    if texto not in peliculas:
+        peliculas.append(texto)
 
 
-def resetear():
-    resumen_mes = []
+def day_film():
+    random = randint(0, peliculas.len)
+    pelicula = peliculas[random]
+    peliculas.pop(random)
+    return pelicula
